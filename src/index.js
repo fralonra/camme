abf5689ee6d0c78efc6b9dc5f951adf282981578
zoron <fralonra@aliyun.com>
2020-10-08 23:44:41 +0800

    simple finger draw

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
 import '@tensorflow/tfjs-backend-webgl';
 
 tfjsWasm.setWasmPath(
-    `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${
-        tfjsWasm.version_wasm}/dist/tfjs-backend-wasm.wasm`);
+  `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/tfjs-backend-wasm.wasm`,
+);
 
 function isMobile() {
   const isAndroid = /Android/i.test(navigator.userAgent);
@@ -13,49 +13,43 @@ function isMobile() {
   return isAndroid || isiOS;
 }
 
-let videoWidth, videoHeight, rafID, ctx, canvas, ANCHOR_POINTS,
-    scatterGLHasInitialized = false, scatterGL, fingerLookupIndices = {
-      thumb: [0, 1, 2, 3, 4],
-      indexFinger: [0, 5, 6, 7, 8],
-      middleFinger: [0, 9, 10, 11, 12],
-      ringFinger: [0, 13, 14, 15, 16],
-      pinky: [0, 17, 18, 19, 20]
-    };  // for rendering each finger as a polyline
+let videoWidth,
+  videoHeight,
+  rafID,
+  ctx,
+  canvas,
+  drawingCtx,
+  drawingCanvas,
+  ANCHOR_POINTS,
+  fingerLookupIndices = {
+    thumb: [0, 1, 2, 3, 4],
+    indexFinger: [0, 5, 6, 7, 8],
+    middleFinger: [0, 9, 10, 11, 12],
+    ringFinger: [0, 13, 14, 15, 16],
+    pinky: [0, 17, 18, 19, 20],
+  }; // for rendering each finger as a polyline
 
 const VIDEO_WIDTH = 640;
 const VIDEO_HEIGHT = 500;
 const mobile = isMobile();
-// Don't render the point cloud on mobile in order to maximize performance and
-// to avoid crowding limited screen space.
-const renderPointcloud = mobile === false;
 
 const state = {
-  backend: 'webgl'
+  backend: 'webgl',
 };
+
+let isDrawing = false
 
 const stats = new Stats();
 stats.showPanel(0);
 document.body.appendChild(stats.dom);
 
-if (renderPointcloud) {
-  state.renderPointcloud = true;
-}
-
 function setupDatGui() {
   const gui = new dat.GUI();
-  gui.add(state, 'backend', ['webgl', 'wasm'])
-      .onChange(async backend => {
-        window.cancelAnimationFrame(rafID);
-        await tf.setBackend(backend);
-        landmarksRealTime(video);
-      });
-
-  if (renderPointcloud) {
-    gui.add(state, 'renderPointcloud').onChange(render => {
-      document.querySelector('#scatter-gl-container').style.display =
-          render ? 'inline-block' : 'none';
-    });
-  }
+  gui.add(state, 'backend', ['webgl', 'wasm']).onChange(async (backend) => {
+    window.cancelAnimationFrame(rafID);
+    await tf.setBackend(backend);
+    landmarksRealTime(video);
+  });
 }
 
 function drawPoint(y, x, r) {
@@ -64,21 +58,32 @@ function drawPoint(y, x, r) {
   ctx.fill();
 }
 
-function drawKeypoints(keypoints) {
-  const keypointsArray = keypoints;
+function drawKeypoint(keypoint) {
+  const [y, x, z] = keypoint;
+  if (z > -60) {
+    if (isDrawing) {
+      isDrawing = false
+    }
+    return;
+  }
+  if (!isDrawing) {
+    drawingCtx.beginPath();
+    drawingCtx.moveTo(y - 2, x - 2);
+    isDrawing = true
+  }
+  drawingCtx.lineTo(y - 2, x - 2);
+  drawingCtx.stroke();
 
-  for (let i = 0; i < keypointsArray.length; i++) {
-    const y = keypointsArray[i][0];
-    const x = keypointsArray[i][1];
+  drawPoint(x - 2, y - 2, 3);
+}
+
+function drawKeypoints(keypoints) {
+  for (let i = 0; i < keypoints.length; i++) {
+    const [y, x, z] = keypoints[i];
     drawPoint(x - 2, y - 2, 3);
   }
 
-  const fingers = Object.keys(fingerLookupIndices);
-  for (let i = 0; i < fingers.length; i++) {
-    const finger = fingers[i];
-    const points = fingerLookupIndices[finger].map(idx => keypoints[idx]);
-    drawPath(points, false);
-  }
+  drawPath(keypoints, false);
 }
 
 function drawPath(points, closePath) {
@@ -100,18 +105,19 @@ let model;
 async function setupCamera() {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     throw new Error(
-        'Browser API navigator.mediaDevices.getUserMedia not available');
+      'Browser API navigator.mediaDevices.getUserMedia not available',
+    );
   }
 
   const video = document.getElementById('video');
   const stream = await navigator.mediaDevices.getUserMedia({
-    'audio': false,
-    'video': {
+    audio: false,
+    video: {
       facingMode: 'user',
       // Only setting the video to a specified size in order to accommodate a
       // point cloud, so on mobile devices accept the default size.
       width: mobile ? undefined : VIDEO_WIDTH,
-      height: mobile ? undefined : VIDEO_HEIGHT
+      height: mobile ? undefined : VIDEO_HEIGHT,
     },
   });
   video.srcObject = stream;
@@ -158,25 +164,27 @@ async function main() {
   ctx.clearRect(0, 0, videoWidth, videoHeight);
   ctx.strokeStyle = 'red';
   ctx.fillStyle = 'red';
-
   ctx.translate(canvas.width, 0);
   ctx.scale(-1, 1);
+
+  drawingCanvas = document.getElementById('drawing');
+  drawingCanvas.width = videoWidth;
+  drawingCanvas.height = videoHeight;
+
+  drawingCtx = drawingCanvas.getContext('2d');
+  drawingCtx.clearRect(0, 0, videoWidth, videoHeight);
+  drawingCtx.lineWidth = 3;
+  drawingCtx.translate(drawingCanvas.width, 0);
+  drawingCtx.scale(-1, 1);
 
   // These anchor points allow the hand pointcloud to resize according to its
   // position in the input.
   ANCHOR_POINTS = [
-    [0, 0, 0], [0, -VIDEO_HEIGHT, 0], [-VIDEO_WIDTH, 0, 0],
-    [-VIDEO_WIDTH, -VIDEO_HEIGHT, 0]
+    [0, 0, 0],
+    [0, -VIDEO_HEIGHT, 0],
+    [-VIDEO_WIDTH, 0, 0],
+    [-VIDEO_WIDTH, -VIDEO_HEIGHT, 0],
   ];
-
-  if (renderPointcloud) {
-    document.querySelector('#scatter-gl-container').style =
-        `width: ${VIDEO_WIDTH}px; height: ${VIDEO_HEIGHT}px;`;
-
-    scatterGL = new ScatterGL(
-        document.querySelector('#scatter-gl-container'),
-        {'rotateOnStart': false, 'selectEnabled': false});
-  }
 
   landmarksRealTime(video);
 }
@@ -185,48 +193,36 @@ const landmarksRealTime = async (video) => {
   async function frameLandmarks() {
     stats.begin();
     ctx.drawImage(
-        video, 0, 0, videoWidth, videoHeight, 0, 0, canvas.width,
-        canvas.height);
+      video,
+      0,
+      0,
+      videoWidth,
+      videoHeight,
+      0,
+      0,
+      canvas.width,
+      canvas.height,
+    );
     const predictions = await model.estimateHands(video);
     if (predictions.length > 0) {
-      const result = predictions[0].landmarks;
-      drawKeypoints(result, predictions[0].annotations);
-
-      if (renderPointcloud === true && scatterGL != null) {
-        const pointsData = result.map(point => {
-          return [-point[0], -point[1], -point[2]];
-        });
-
-        const dataset =
-            new ScatterGL.Dataset([...pointsData, ...ANCHOR_POINTS]);
-
-        if (!scatterGLHasInitialized) {
-          scatterGL.render(dataset);
-
-          const fingers = Object.keys(fingerLookupIndices);
-
-          scatterGL.setSequences(
-              fingers.map(finger => ({indices: fingerLookupIndices[finger]})));
-          scatterGL.setPointColorer((index) => {
-            if (index < pointsData.length) {
-              return 'steelblue';
-            }
-            return 'white';  // Hide.
-          });
-        } else {
-          scatterGL.updateDataset(dataset);
-        }
-        scatterGLHasInitialized = true;
+      const keypoints = predictions[0].annotations.indexFinger;
+      drawKeypoint(keypoints[3]);
+      drawKeypoints(keypoints);
+    } else {
+      if (isDrawing) {
+        isDrawing = false
       }
     }
     stats.end();
     rafID = requestAnimationFrame(frameLandmarks);
-  };
+  }
 
   frameLandmarks();
 };
 
-navigator.getUserMedia = navigator.getUserMedia ||
-    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
 
 main();
