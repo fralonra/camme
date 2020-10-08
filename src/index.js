4bcc2f72a7bbc197f227949f589ed7eaee738f74
zoron <fralonra@aliyun.com>
2020-10-08 20:29:37 +0800

    clean project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-navigator.mediaDevices.getUserMedia({
-  video: true
-})
-.then(function (stream) {
-  const video = document.getElementById('video')
-  video.srcObject = stream
-  video.onloadedmetadata = function (e) {
-    video.play();
-  }
-})
-.catch(function (err) {
-  /* 处理error */
-});
+console.log('hello')
